8abd5c53ec9dd40af23351032e97f0216bf1bcf6
Daniel Mocan <dane1505@gmail.com>
2022-09-09 14:02:09 +0300

    Added images and data fetching

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import { AppRoutes } from "./AppRoutes/AppRoutes";
 
 export const App = () => {
-  return <AppRoutes />;
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+        <AppRoutes />
+    </QueryClientProvider>
+  );
 };
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,16 @@
+import {useQuery} from "@tanstack/react-query";
 import "../App.css";
-
+import apiService from "../utils/apiService";
 import { Header } from "../Components/Header/Header";
 
 export const Home = () => {
+  const { data: restaurants, isLoading } = useQuery(['restaurants'], apiService.getRestaurants);
+  console.log('restaurants ', restaurants);
   return (
     <div className="App">
       <Header />
+      {isLoading && <div>Loading...</div>}
+      { restaurants && <ul>{restaurants.map( restaurant => <li key={restaurant.id}>{restaurant.name} <img src={`/images/${restaurant.logo}`} /></li>)}</ul>}
     </div>
   );
 };
