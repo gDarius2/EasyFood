085c81d1c82fbf97ab9d7962e031a26abefa81e0
TatarAlex <alex.tatar@fortech.ro>
2022-09-09 14:49:30 +0300

    added company cards

--- src/Components/CompanyCard/CompanyCard.jsx
+++ src/Components/CompanyCard/CompanyCard.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import "./CompanyCard.css";
 
-const CompanyCard = () => {
+const CompanyCard = ({ company }) => {
+  const { logo, name, address, offerHours } = company;
   return (
     <div className="card-container">
-      <div className="card-image">IMAGE</div>
-      <h1 className="card-company">McDonalds</h1>
-      <p className="company-address">Street number 65</p>
+      <div className="card-image">
+        <img src={`images/${logo}`} alt={name} />
+      </div>
+      <h1 className="card-company">{name}</h1>
+      <p className="company-address">{address}</p>
       <div className="pick-up-time">
         <span>Pick-up time:</span>
-        <span className="time">08:00 AM - 22:00 PM</span>
+        <span className="time">{offerHours}</span>
       </div>
       <p className="offers">Only a few offers left, hurry up!</p>
       <button className="card-button">View</button>
